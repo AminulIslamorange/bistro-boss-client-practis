5707343322afa8c0cac668cc6bcc35732e281cef
AminulIslamorange <aminulislamorange@gmail.com>
2024-11-14 23:42:55 +0600

    review added

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,13 +1,33 @@
 import FoodCard from "../../../components/FoodCard/FoodCard";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 
 const OrderTab = ({items}) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      };
+    
     return (
-        <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6'>
-     {
+        <div >
+    
+    
+     <Swiper
+     pagination={pagination}
+     modules={[Pagination]}
+     className="mySwiper"
+   >
+     <SwiperSlide> <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6'>{
     items.map(item=><FoodCard key={item._id} item={item}></FoodCard>)
-    } 
-    </div>
+    }</div> </SwiperSlide>
+     
+   </Swiper>
+   </div>
     );
 };
 
